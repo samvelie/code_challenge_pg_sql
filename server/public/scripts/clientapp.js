2a0deea8502a97ad22db38c946a0e71c2dffbc32
Millie Walsh <millie@primeacademy.io>
2016-09-18 23:58:35 -0500

    remove delete and put functionality

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+  // get treats on load
+  getTreats();
+
   /**---------- Event Handling ----------**/
   $('#searchButton').on('click', function (event) {
     event.preventDefault();
@@ -9,46 +12,6 @@ $(document).ready(function () {
     searchTreats(queryString);
   });
 
-  $('#modeToggle').on('click', function (event) {
-    event.preventDefault();
-    toggleView();
-  });
-
-  $('#treat-display').on('click', '.edit', function () {
-    var $treat = $(this).closest('.individual-treat');
-    var Treat = {
-      id: $treat.data('id'),
-      name: $.trim($treat.find('h3').text()),
-      description: $.trim($treat.find('p').text()),
-      pic: $treat.find('img').attr('src'),
-    };
-
-    displayEditModal(Treat);
-  });
-
-  $('#treat-display').on('click', '.delete', function (event) {
-    var $treat = $(this).closest('.individual-treat');
-    deleteTreat($treat.data('id'));
-  });
-
-  $('#modal-save').on('click', function (event) {
-    event.preventDefault();
-
-    $('#modal').hide();
-    var id = $('#modal').data('id');
-    var treat = {
-      name: $('#treat-name').val(),
-      description: $('#treat-description').val(),
-      pic: $('#treat-url').val(),
-    };
-
-    putTreat(id, treat);
-  });
-
-  $('#modal-cancel').on('click', function (event) {
-    event.preventDefault();
-    $('#modal').hide();
-  });
   /**---------- AJAX Functions ----------**/
 
   // GET /treats
@@ -95,38 +58,16 @@ $(document).ready(function () {
     });
   }
 
-  // PUT /treats/1
-  function putTreat(treatId, treat) {
-    $.ajax({
-      method: 'PUT',
-      url: '/treats/' + treatId,
-      data: treat,
-    })
-    .done(function () {
-      console.log('PUT /treats/', treatId);
-
-      getTreats();
-    });
-  }
-
-  // DELETE /treats/1
-  function deleteTreat(treatId) {
-    $.ajax({
-      method: 'DELETE',
-      url: '/treats/' + treatId,
-    })
-    .done(function () {
-      console.log('DELETE /treats/', treatId);
-
-      getTreats();
-    });
-  }
   /** ---------- DOM Functions ----------**/
   function appendTreat(treat) {
-
+    console.log(treat);
     // append a treat to the DOM and add data attributes
     // treat-display -> treat row -> treat
+    var $treats = $('#treat-display');
+    console.log($treats);
+
     var treatCount = $treats.children().children().length;
+    console.log(treatCount);
 
     if (treatCount % 2 === 0) {
       // add a treat row
@@ -135,7 +76,7 @@ $(document).ready(function () {
 
     var $treat = $('<div class="six columns individual-treat">' +
                   '<div class="image-wrap">' +
-                  '<img src="' + treat.pic + '" class="u-max-full-width" />' +
+                  '<img src="' + treat.url + '" class="u-max-full-width" />' +
                   '<div class="toggle row">' +
                   '<div class="six columns">' +
                   '<button class="edit u-full-width">Edit</button>' +
@@ -151,30 +92,6 @@ $(document).ready(function () {
 
     $treat.data('id', treat.id);
 
-    $('treat-row:last-of-type').append($treat);
+    $('.treat:last-of-type').append($treat);
   }
-
-  // show/hide edit buttons
-  function toggleView() {
-    if ($('#modeToggle').text() == 'View') {
-      // change button to Edit
-      $('#modeToggle').text('Edit');
-    } else {
-      //change button to View
-      $('#modeToggle').text('View');
-    }
-
-    $('.toggle').toggle();
-  }
-
-  function displayEditModal(treat) {
-    $('#modal').show();
-
-    // populate with data
-    $('#treat-name').val(treat.name);
-    $('#treat-description').val(treat.description);
-    $('#treat-url').val(treat.pic);
-    $('#modal').data('id', treat.id);
-  }
-
 });
