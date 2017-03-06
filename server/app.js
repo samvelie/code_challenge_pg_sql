59616daec8b7602afa22bed4f452a5e754a023a9
Kris Szafranski <kdszafranski@gmail.com>
2017-03-06 08:57:44 -0600

    commented node server

--- server/app.js
+++ server/app.js
@@ -6,11 +6,15 @@ var port = process.env.PORT || 3000;
 /*** Build out a module to manage our treats requests. ***/
 
 
+
+
+// Get static files
+app.use(express.static('./server/public'));
+
+// Get index.html
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./server/public/views/index.html'));
 });
-
-app.use(express.static('./server/public'));
 
 
 app.listen(port, function() {
